bf3369cb293de29525457f3ebc312898e2cfe001
Rodrigo <alu0101154473@ull.edu.es>
2023-11-26 18:51:27 +0000

    Diferentes hilos api_service para logs-json e imprimir pantalla y confirmado valores aterrizaje

--- api_service.py
+++ api_service.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import threading
 import time
 import json
 from FlightRadar24.api import FlightRadar24API  # Asegúrate de importar adecuadamente tu módulo FlightRadar24
@@ -10,28 +11,34 @@ logging.basicConfig(filename='api_service.log', level=logging.DEBUG)
 
 # Datos iniciales
 datos_json = {}
+lock = threading.Lock()
 
 @app.route('/obtener_json', methods=['GET'])
 def obtener_json():
-    return jsonify(datos_json)
+    with lock:
+        return jsonify(datos_json)
 
 def actualizar_datos_json():
     global datos_json
     fr_api = FlightRadar24API()
-    
-    # Lógica para obtener datos actualizados
-    key_flights = fr_api.get_flights(
-        registration="HA-LGD"
-    )
 
-    if key_flights:
-        detalles_vuelo = fr_api.get_flight_details(key_flights[0])
-        datos_json = detalles_vuelo.get("time")
-        detalles_vuelo.get("time")["real"]["departure"]
+    while True:
+        # Lógica para obtener datos actualizados
+        key_flights = fr_api.get_flights(
+            registration="EC-MPI"
+        )
 
+        if key_flights:
+            detalles_vuelo = fr_api.get_flight_details(key_flights[0])
+            print(key_flights[0])
+            with lock:
+                datos_json = detalles_vuelo.get("time")
+        
+        time.sleep(3)
 
 if __name__ == '__main__':
-    while True:
-        actualizar_datos_json()
-        time.sleep(3)
-        app.run(debug=True, port=5000, use_reloader=False)
+    hilo_flask = threading.Thread(target=app.run, kwargs={'debug': True, 'port': 5000, 'use_reloader': False})
+    hilo_flask.start()
+
+    hilo_datos = threading.Thread(target=actualizar_datos_json)
+    hilo_datos.start()
--- pruebas.py
+++ pruebas.py
@@ -8,43 +8,6 @@ from io import BytesIO
 from PIL import Image
 from flask import Flask, jsonify
 
-
-# app = Flask(__name__)
-
-# # Datos iniciales
-# datos_json = {'clave': 'valor', 'otra_clave': 'otro_valor'}
-
-# @app.route('/obtener_json', methods=['GET'])
-# def obtener_json():
-#     return jsonify(datos_json)
-
-# def actualizar_datos_json():
-#     global datos_json
-#     # Aquí puedes realizar cualquier lógica para actualizar los datos_json
-#     # Por ejemplo, puedes obtener datos de una API externa, base de datos, etc.
-
-# # Bucle infinito con temporizador para actualizar los datos_json cada 60 segundos
-# if __name__ == '__main__':
-#     while True:
-#         actualizar_datos_json()
-#         time.sleep(3)  # Espera 60 segundos antes de la próxima actualización
-#         app.run(debug=True, port=5000, use_reloader=False)
-
-#from FlightRadar24 import Airport
-
-# Obtener los bytes de la bandera del país
-# flag_bytes = FlightRadar24API().get_country_flag("spain")[0]
-
-# # Convertir los bytes SVG a PNG usando cairosvg
-# png_bytes = cairosvg.svg2png(flag_bytes)
-
-# # Crear un objeto BytesIO desde los bytes PNG
-# png_io = BytesIO(png_bytes)
-
-# # Utilizar png_io con las funciones de visualización o manipulación según tus necesidades
-# png_image = Image.open(png_io)
-# png_image.show()
-# exit
 while True:
     fr_api = FlightRadar24API()
     #print(fr_api.get_airport("TFN").country)
@@ -52,13 +15,12 @@ while True:
     count = 0
     key_flights = fr_api.get_flights(
         #airline = "CAI",
-        registration = "EI-DHA"
+        registration = "EC-MUJ"
     )
-    #print(len(key_flights))
-    fr_api.get_country_flag("spain")
-
-    #print(key_flights[0])
-    arch_j_son = json.dumps(fr_api.get_flight_details(key_flights[0]).get("time"))
+    detalles_vuelo = fr_api.get_flight_details(key_flights[0])
+    print(detalles_vuelo.get("time")["real"]["arrival"],end="\t")
+    print(key_flights[0])
+    time.sleep(3)
 for i in key_flights:
     #if "XR" in i.number:
     print(i.id,i.callsign,i.number,
